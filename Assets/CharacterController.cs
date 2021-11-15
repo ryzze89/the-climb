bb77163f488fc58bb55b044266a24001fff28b85
Wisley Vergara <wvergara2021@elam.co.uk>
2021-11-15 10:37:33 +0000

    Added physics-based player movement and jump mechanic

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -10,31 +10,40 @@ public class CharacterController : MonoBehaviour
     float rotationSpeed = 2.0f;
     public float camRotationSpeed = 1.5f;
     GameObject cam;
+    Rigidbody myRigidBody;
+
+    public bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
 
 
     // Start is called before the first frame update
     void Start()
     {
         cam = GameObject.Find("Main Camera");
-
+        myRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal")* maxSpeed);
-        rotation = rotation + Input.GetAxis("Mouse X");
+        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
 
-        camRotation = camRotation + Input.GetAxis("Mouse Y");
-        cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
-
-        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
         cam.transform.localRotation = Quaternion.Euler(new Vector3(-camRotation, 0.0f, 0.0f));
 
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        myRigidBody.velocity = new Vector3(newVelocity.x, myRigidBody.velocity.y, newVelocity.z);
 
-
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.3f, groundLayer);
         
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.AddForce(transform.up * jumpForce);
+        }
+
+        Vector3 newVelocity = (tranforms.formard * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
     }
 }
