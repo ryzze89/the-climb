943dab4da3f2896db1d7572495bc80ee72223fce
Wisley Vergara <wvergara2021@elam.co.uk>
2021-11-16 15:52:37 +0000

    i added a sprint

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -16,13 +16,18 @@ public class CharacterController : MonoBehaviour
     public GameObject groundChecker;
     public LayerMask groundLayer;
     public float jumpForce = 300.0f;
+    public float normalSpeed = 10.0f;
+    public float sprintSpeed = 20.0f;
 
+    public float maxSprint = 5.0f;
+    float sprintTimer;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = GameObject.Find("Main Camera");
         myRigidBody = GetComponent<Rigidbody>();
+        sprintTimer = maxSprint;
     }
 
     // Update is called once per frame
@@ -34,7 +39,8 @@ public class CharacterController : MonoBehaviour
         camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
         cam.transform.localRotation = Quaternion.Euler(new Vector3(-camRotation, 0.0f, 0.0f));
 
-        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+
         myRigidBody.velocity = new Vector3(newVelocity.x, myRigidBody.velocity.y, newVelocity.z);
 
         isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.3f, groundLayer);
@@ -44,6 +50,20 @@ public class CharacterController : MonoBehaviour
             myRigidBody.AddForce(transform.up * jumpForce);
         }
 
-        Vector3 newVelocity = (tranforms.formard * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+
+        if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0.0f)
+        {
+            maxSpeed = sprintSpeed;
+                sprintTimer = sprintTimer - Time.deltaTime;
+        } else
+        {
+            maxSpeed = normalSpeed;
+            if (Input.GetKey(KeyCode.LeftShift) == false)
+            {
+                sprintTimer = sprintTimer + Time.deltaTime;
+            }
+        }
+
+        sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, maxSprint);
     }
 }
