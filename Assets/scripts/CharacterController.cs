b742ed72cf2c0c4eddc5b390cc927a84c1493131
Wisley Vergara <wvergara2021@elam.co.uk>
2021-11-24 10:43:15 +0000

    added animations

---
+++ Assets/scripts/CharacterController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float maxSpeed = 100.0f;
+    float rotation = 0.0f;
+    float camRotation = 0.0f;
+    float rotationSpeed = 2.0f;
+    public float camRotationSpeed = 1.5f;
+    GameObject cam;
+    Rigidbody myRigidBody;
+
+    public bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
+    public float normalSpeed = 10.0f;
+    public float sprintSpeed = 20.0f;
+
+    public float maxSprint = 5.0f;
+    float sprintTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+        myRigidBody = GetComponent<Rigidbody>();
+        sprintTimer = maxSprint;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+
+        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        camRotation = Mathf.Clamp(camRotation, -40.0f, 40.0f);
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(-camRotation, 0.0f, 0.0f));
+
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+
+        myRigidBody.velocity = new Vector3(newVelocity.x, myRigidBody.velocity.y, newVelocity.z);
+
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.3f, groundLayer);
+        
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.AddForce(transform.up * jumpForce);
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0.0f)
+        {
+            maxSpeed = sprintSpeed;
+                sprintTimer = sprintTimer - Time.deltaTime;
+        } else
+        {
+            maxSpeed = normalSpeed;
+            if (Input.GetKey(KeyCode.LeftShift) == false)
+            {
+                sprintTimer = sprintTimer + Time.deltaTime;
+            }
+        }
+
+        sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, maxSprint);
+    }
+
+
+
+
+}
